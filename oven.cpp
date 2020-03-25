97a50521e4593f729be69417ed066e489c72f112
S121B-46586 <56250876+S121B-46586@users.noreply.github.com>
2020-03-25 22:21:12 +0100

    Update oven.cpp

--- oven.cpp
+++ oven.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+class Clock
+{
+	int minute = 00, hour = 12;
+	int day = 13, month = 01, year = 2020;
+
+public:
+	Clock(int minute, int hour)
+	{
+		this->minute = minute;
+		this->hour = hour;
+	}
+
+};
+
 class Oven
 {
 
@@ -13,29 +27,29 @@ class Oven
 	string modeList[5] = { "termoobieg", "grill", "obustronnie", "rozmrazanie", "podgrzewanie" };
 	string mode = "brak";
 
-	
-
+	Clock* clock;
 
 
 public:
 	
 	Oven() : Oven(0, 0, "Brak"){} 
 
-	Oven(int temperature, int time, string mode) : 
+	Oven(int temperature, int time, string mode, Clock * clock = new Clock(00, 12)) : 
 		temperature{(temperature > 200)? temperature = 200 : temperature}
 		//sprawdza, czy temperatura jest wyzsza niz 200 stopni, jak to to zmiena na 200
 	{
-		this->temperature = temperature;
 		this->time = time;
 		this->mode = mode;
-		
-		
+		this->clock = clock;	
 	}
+
 
 	Oven(int temperature, int time) : Oven(temperature, time, "Brak") //odsyla do konstruktora 2
 	{
 		wentylacja = 0;
 	}
+
+
 
 	void setTemperature(int temperature)
 	{
@@ -61,19 +75,7 @@ public:
 
 	//Klasa Clock, zegarek w piekarniku
 
-	class Clock
-	{
-		int minute = 00, hour = 12;
-		int day = 13, month = 01, year = 2020;
 
-	public:
-		Clock(int minute, int hour)
-		{
-			this->minute = minute;
-			this->hour = hour;
-		}
-		
-	};
 };
 
 
@@ -82,19 +84,19 @@ public:
 
 int main()
 { 
-	Oven* c1 = new Oven(300, 40, "Termoobieg"); Oven k1 = *c1;
-	Oven* c2 = new Oven; Oven k2 = *c2;
-	Oven* c3 = new Oven(300, 300); Oven k3 = *c3;
-	k2.getInfo();
-	k2.setTemperature(90);
-	k2.setMode("rozmrazanie");
-	k2.setTime(30);
-	k2.getInfo();
-	k1.printModes();
+	Oven* c1 = new Oven(300, 40, "Termoobieg"); 
+	Oven* c2 = new Oven; 
+	Oven* c3 = new Oven(300, 300);
+	c2->getInfo();
+	c2->setTemperature(90);
+	c2->setMode("rozmrazanie");
+	c2->setTime(30);
+	c2->getInfo();
+	c1->printModes();
 	cout << endl;
-	k2.getInfo();
+	c2->getInfo();
 	cout << endl;
-	k3.getInfo();
+	c2->getInfo();
 	delete c1; c1 = nullptr;
 	delete c2; c2 = nullptr;
 	delete c3; c3 = nullptr;
